f923a8e93b1522ff3adf21fdc0416544657ee97d
victorioc <victoriocabrera@gmail.com>
2018-10-02 16:05:52 -0400

    dynamically fill blog posts and also some extra func with author hyperlinks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ApplicationController
   def post
-
+    @title = Post.find(params[:id]).title
+    @body = Post.find(params[:id]).body
+    @author = Post.find(params[:id]).author.name
+    @author_for_wiki = @author.gsub(/w+/, "_")
   end
 
   def search
+    layout  :"application-alt-header"
   end
 
   def results
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/post'
+  get 'pages/post/:id', to: 'pages#post'
   get 'pages/search'
   get 'pages/results'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
